ea4dd9128966be14676ea55d6bb27985baf5beaa
Ken chan <kchan27@ford.com>
2023-11-29 09:52:23 +0000

    switched lava lamp video to firebase hosting

--- app/(routes)/calm/page.jsx
+++ app/(routes)/calm/page.jsx
@@ -5,24 +5,23 @@ import { useRef } from "react";
 export default function Page() {
   const refVideo = useRef();
   return (
-    <div className="min-w-screen min-h-screen">
-      <video
-        autoPlay
-        muted
-        controls
-        loop
-        playsInline
-        className="w-1/2 h-1/2"
-        ref={refVideo}
-        onClick={() => {
-          refVideo.current.muted = false;
-          console.log(refVideo);
-        }}
-      >
-        <source src="/videos/lavalamp2.mp4" type="video/mp4" />
-        <source src="/videos/lavalamp2.webm" type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    <video
+      autoPlay
+      muted
+      loop
+      playsInline
+      className="fixed min-w-screen min-h-screen"
+      ref={refVideo}
+      onClick={() => {
+        refVideo.current.muted = false;
+        console.log(refVideo);
+      }}
+    >
+      <source
+        src="https://firebasestorage.googleapis.com/v0/b/ken-demo-d95fe.appspot.com/o/videos%2FlavaLamp2.webm?alt=media&token=4d86fcd8-868a-4735-bd56-68ec00dacd03"
+        type="video/webm"
+      />
+      Your browser does not support the video tag.
+    </video>
   );
 }
