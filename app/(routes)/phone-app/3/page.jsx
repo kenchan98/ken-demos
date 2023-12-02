eac1a1fce53c53036b97d1822056a11b92af43ae
Ken chan <kchan27@ford.com>
2023-12-02 21:18:10 +0000

    disable pull to refresh

--- app/(routes)/phone-app/3/page.jsx
+++ app/(routes)/phone-app/3/page.jsx
@@ -32,7 +32,7 @@ export default function CameraPage() {
 
 
   */
-  function onClickBackButtonGallery() {
+  function onClickCloseButton() {
     setShowGallery(false);
   }
   function onPressGalleryIcon() {
@@ -77,41 +77,34 @@ export default function CameraPage() {
           className="flex text-center m-8 text-4xl font-bold"
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
-          transition={{ delay: 5 }}
+          transition={{ delay: 2 }}
         >
           Take photos of the crash scene
         </motion.h1>
       </div>
       {/* ====== BOTTOM ====== */}
-      <div className="flex fixed bottom-0 items-center justify-center h-1/6 w-full bg-gray-900">
-        <div className="w-1/3"></div>
-        {imgDataList.length < 3 ? (
-          <CameraButton onPressed={capturePress} />
-        ) : (
-          <Link href="./4">
-            <Button>Next</Button>
-          </Link>
-        )}
-        <div className="w-1/3" onClick={onPressGalleryIcon}>
-          {imgDataList.length > 0 && <IconThumbs imgDataList={imgDataList} />}
+      {!showGallery && (
+        <div className="flex fixed bottom-0 items-center justify-center h-1/6 w-full bg-gray-900">
+          <div className="w-1/3"></div>
+          {imgDataList.length < 30 ? (
+            <CameraButton onPressed={capturePress} />
+          ) : (
+            <Link href="./4">
+              <Button>Next</Button>
+            </Link>
+          )}
+          <div className="w-1/3" onClick={onPressGalleryIcon}>
+            {imgDataList.length > 0 && <IconThumbs imgDataList={imgDataList} />}
+          </div>
         </div>
-      </div>
+      )}
 
-      {/* ====== GALLERY ====== */
-      /*<motion.div
-        initial={{ opacity: 0, y: 0 }}
-        animate={{ opacity: 1, y: -900 }}
-        className="relative block top-1/2 w-full h-full bg-red-300 "
-      >
-        <div className="absolute block w-1/2 h-full bg-green-300/50">
-          GALLERY
-        </div>
-  </motion.div>*/}
+      {/* ====== GALLERY ====== */}
       {showGallery && (
-        <div className="absolute w-full  bg-green-300/50">
+        <div className="absolute w-full h-screen bg-black">
           <Gallery
             imgDataList={imgDataList}
-            onClickBackButton={onClickBackButtonGallery}
+            onClickCloseButton={onClickCloseButton}
           />
         </div>
       )}
--- app/_components/gallery.js
+++ app/_components/gallery.js
@@ -2,25 +2,24 @@ import PhoneAppHeader from "./header-app";
 
 export default function Gallery(props) {
   return (
-    <div className="flex flex-col w-full h-full bg-gray-800">
+    <>
       {/* ====== TOP ====== */}
       <PhoneAppHeader
         title="Photos"
-        onClickBackButton={props.onClickBackButton}
+        buttonClose
+        onClickCloseButton={props.onClickCloseButton}
       />
       {/* ====== MIDDLE ====== */}
-      <div className="flex w-full min-h-screen h-fit flex-wrap ">
-        <div className="flex flex-wrap">
-          {props.imgDataList.map((imgData, index) => (
-            <img
-              key={index}
-              className="w-1/2 p-2 object-cover h-1/3"
-              src={imgData}
-            />
-          ))}
-        </div>
-        <div className="grow"></div>
+      <div className="flex flex-wrap">
+        {props.imgDataList.map((imgData, index) => (
+          <img
+            key={index}
+            className="w-1/2 p-2 object-cover h-1/3"
+            src={imgData}
+          />
+        ))}
       </div>
-    </div>
+      <div className="h-1/6 w-full bg-black"></div>
+    </>
   );
 }
